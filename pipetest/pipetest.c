22a3cf53eac9bf522aa65d8101e819c584b57cc0
Dan <dan@localhost.localdomain>
2021-01-26 22:59:55 +1100

    Ok, it works, let's add stderr.

--- pipetest/pipetest.c
+++ pipetest/pipetest.c
@@ -6,7 +6,7 @@
    stdin and the other for child's stdout to flow to
    parent's stdin */
  
-#define NUM_PIPES          2
+#define NUM_PIPES          3 
  
 #define PARENT_WRITE_PIPE  0
 #define PARENT_READ_PIPE   1
@@ -18,14 +18,13 @@ int pipes[NUM_PIPES][3];
 #define READ_FD  0
 #define WRITE_FD 1
  
-#define PARENT_READ_FD  ( pipes[PARENT_READ_PIPE][READ_FD]   )
-#define PARENT_WRITE_FD ( pipes[PARENT_WRITE_PIPE][WRITE_FD] )
+#define PARENT_STDOUT_FD ( pipes[PARENT_READ_PIPE][READ_FD]   )
+#define PARENT_STDIN_FD  ( pipes[PARENT_WRITE_PIPE][WRITE_FD] )
  
-#define CHILD_STDIN   ( pipes[PARENT_WRITE_PIPE][READ_FD]  )
-#define CHILD_STDOUT  ( pipes[PARENT_READ_PIPE][WRITE_FD]  )
+#define CHILD_STDIN_FD   ( pipes[PARENT_WRITE_PIPE][READ_FD]  )
+#define CHILD_STDOUT_FD  ( pipes[PARENT_READ_PIPE][WRITE_FD]  )
  
-void
-main()
+void main()
 {
     int outfd[2];
     int infd[2];
@@ -36,35 +35,35 @@ main()
      
     if(!fork()) {
  
-        dup2(CHILD_STDIN, STDIN_FILENO);
-        dup2(CHILD_STDOUT, STDOUT_FILENO);
+        dup2(CHILD_STDIN_FD, STDIN_FILENO);
+        dup2(CHILD_STDOUT_FD, STDOUT_FILENO);
  
         /* Close fds not required by child. Also, we don't
            want the exec'ed program to know these existed */
-        close(CHILD_STDIN);
-        close(CHILD_STDOUT);
-        close(PARENT_READ_FD);
-        close(PARENT_WRITE_FD);
+        close(CHILD_STDIN_FD);
+        close(CHILD_STDOUT_FD);
+        close(PARENT_STDOUT_FD);
+        close(PARENT_STDIN_FD);
 
-	fprintf(stdout, "hello, stdout\n");
+	fprintf(stdout, "hello\n");
           
     } else {
         char buffer[100];
         int count;
  
         /* close fds not required by parent */       
-        close(CHILD_STDIN);
-        close(CHILD_STDOUT);
+        close(CHILD_STDIN_FD);
+        close(CHILD_STDOUT_FD);
  
         // Write to child’s stdin
   
         // Read from child’s stdout
-        count = read(PARENT_READ_FD, buffer, sizeof(buffer)-1);
-        if (count >= 0) {
+        count = read(PARENT_STDOUT_FD, buffer, sizeof(buffer)-1);
+        if (count > 0) {
             buffer[count] = 0;
             printf("1> %s", buffer);
         } else {
-            printf("IO Error\n");
+            printf("received %d bytes from child's stdout\n", count);
         }
     }
 }
